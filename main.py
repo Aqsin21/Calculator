1a36a51f069c1048546db9d63756c52eb73e6b72
Aqsin21 <150579577+Aqsin21@users.noreply.github.com>
2025-01-29 21:17:56 +0400

    'test2'

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-operator = input("Enter an operator (+ - * /):")
-num1 = float(input("Enter the 1th number:"))
-num2= float(input("Enter the 2th number:"))
+operator = input("Enter an operator (+ - * /): ")
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
 
-if operator =="+":
-   result= num1+num2
-   print(result)
-elif operator =="-":
-   result=num1-num2
-   print(result)
-elif operator =="*":
-   result=num1*num2
-   print(result)
+if operator == "+":
+    print("Result:", num1 + num2)
+elif operator == "-":
+    print("Result:", num1 - num2)
+elif operator == "*":
+    print("Result:", num1 * num2)
+elif operator == "/":
+    if num2 != 0:
+        print("Result:", num1 / num2)
+    else:
+        print("Error: Division by zero is not allowed.")
+else:
+    print("Invalid operator! Please enter one of (+, -, *, /).")
 
-elif operator =="/":
-   result = num1/num2
-   print(result)
          
        
