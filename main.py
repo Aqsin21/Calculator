748029f18dcfc89860b20c4db48af111b563cd4a
Aqsin21 <150579577+Aqsin21@users.noreply.github.com>
2025-01-29 21:08:39 +0400

    'test1'

---
+++ main.py
@@ -0,0 +1,19 @@
+operator = input("Enter an operator (+ - * /):")
+num1 = float(input("Enter the 1th number:"))
+num2= float(input("Enter the 2th number:"))
+
+if operator =="+":
+   result= num1+num2
+   print(result)
+elif operator =="-":
+   result=num1-num2
+   print(result)
+elif operator =="*":
+   result=num1*num2
+   print(result)
+
+elif operator =="/":
+   result = num1/num2
+   print(result)
+         
+       
